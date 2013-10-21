4d9855684b58421f9bd45bdbc0e9f61caaac0251
schulzca <coreyaschulz@gmail.com>
2013-10-21 00:08:24 -0500

    new init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fdtgroup::Application.config.secret_token = '3057c53b0fc86375afa3d1c08111bcc3f7fd4e5918fb2969a27920090ffd7fc22997e9e3937abef55771086eeef3d950b826561036c642514cca419dd1d55d89'
